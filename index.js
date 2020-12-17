83f0a2a6c56fb6a3931ad4ae4410f6cfb7a5d5ad
ikeaohnea <ikeaohnea@gmail.com>
2020-12-17 15:40:17 +0100

    covid Data

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const app = express()
 const port = 3000
 const fs = require('fs')
 
-const data = fs.readFileSync('todo.json', 'utf8')
-const todoList = JSON.parse(data)
+//for todolist
+// const data = fs.readFileSync('todo.json', 'utf8')
+// const todoList = JSON.parse(data)
+// app.get('/', (req, res) => {
+//   res.send(todoList)
+// })
 
-app.get('/', (req, res) => {
-  res.send(todoList)
+//code for covid data
+const data = fs.readFileSync('covid.json', 'utf8')
+const covidData = JSON.parse(data)
+
+app.get('/covidCases', (req, res) => {
+  const country = req.query.country
+  
+  const result = []
+
+  for (let record of covidData.records){
+    if(record.countriesAndTerritories == country){
+      result.push(record)
+    }
+  }
+  res.json(result)
+  // res.send(covidData)
 })
 
 app.listen(port, () => {
